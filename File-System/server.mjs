6ed206c1ef29985d42bf6fd4a4c9702bab437186
niamul007 <am3994566@gmail.com>
2026-01-24 11:32:51 +0600

    adding serving dir name

--- File-System/server.mjs
+++ File-System/server.mjs
@@ -1,16 +1,12 @@
 import http from 'node:http';
-import { cwd } from 'node:process';
+import { serveStaticFile } from './Utilities/serveStatic.mjs';
 
 const PORT = 5000;
 const HOSTNAME = 'localhost';
 const __dirname = import.meta.dirname;
 
-console.log(`Absolute path of current directory: ${__dirname}`);
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+const server = http.createServer(async (req, res) => {
+  await serveStaticFile(__dirname, res);
 });
 
 server.listen(PORT, HOSTNAME, () => {
