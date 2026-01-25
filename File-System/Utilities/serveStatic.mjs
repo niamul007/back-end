04c5af5a1aff60dd9bb7aa383812eb39460aabe0
niamul007 <am3994566@gmail.com>
2026-01-25 20:18:52 +0600

    adding different file with extation name

--- File-System/Utilities/serveStatic.mjs
+++ File-System/Utilities/serveStatic.mjs
@@ -1,12 +1,17 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
+import { contentType } from './contentType.js';
 
-export async function serveStaticFile(baseDir, res) {
+export async function serveStaticFile(baseDir, res, req) {
   try {
-    const filePath = path.join(baseDir, 'Public', 'index.html');
+    const publicDir = path.join(baseDir, 'Public');
+    const filePath = path.join(publicDir, req.url === '/' ? 'index.html' : req.url);
+    const fileExtension = path.extname(filePath);
+    const contentTypeValue = contentType(fileExtension);
+
     const fileContent = await fs.readFile(filePath, 'utf-8');
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', contentTypeValue);
     res.end(fileContent);
   } catch (error) {
     console.error('Error serving file:', error.message);
