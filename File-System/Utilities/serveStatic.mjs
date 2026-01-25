ae735cb78abfca667e90fe327cacc1ea871c5f4a
niamul007 <am3994566@gmail.com>
2026-01-25 21:04:23 +0600

    adding helper fn

--- File-System/Utilities/serveStatic.mjs
+++ File-System/Utilities/serveStatic.mjs
@@ -1,6 +1,7 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 import { contentType } from "./contentType.js";
+import { sendJSON } from "./sendJSON.mjs";
 
 export async function serveStaticFile(baseDir, res, req) {
   try {
@@ -11,24 +12,19 @@ export async function serveStaticFile(baseDir, res, req) {
     );
     const fileExtension = path.extname(filePath);
     const contentTypeValue = contentType(fileExtension);
-
     const fileContent = await fs.readFile(filePath, "utf-8");
-    res.statusCode = 200;
-    res.setHeader("Content-Type", contentTypeValue);
-    res.end(fileContent);
+    sendJSON(res,fileContent,200,contentTypeValue);
+
   } catch (error) {
     if (error.code === "ENOENT") {
       const errorPath = path.join(baseDir, "Public", "404.html");
       const errorContent = await fs.readFile(errorPath, "utf-8");
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html");
-      res.end(errorContent);
+      sendJSON(res,errorContent,404,"text/html");
       return;
     } else {
       console.error("Error serving file:", error.message);
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("404 - File Not Found");
+      sendJSON(res,{ error: "Internal Server Error" },500,"application/json");
+      return;
     }
   }
 }
