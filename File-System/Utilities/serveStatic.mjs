0e04abfcde6fb18c9681409bc5518dbc947c0708
niamul007 <am3994566@gmail.com>
2026-01-25 20:48:38 +0600

    adding error page

--- File-System/Utilities/serveStatic.mjs
+++ File-System/Utilities/serveStatic.mjs
@@ -1,22 +1,34 @@
-import path from 'node:path';
-import fs from 'node:fs/promises';
-import { contentType } from './contentType.js';
+import path from "node:path";
+import fs from "node:fs/promises";
+import { contentType } from "./contentType.js";
 
 export async function serveStaticFile(baseDir, res, req) {
   try {
-    const publicDir = path.join(baseDir, 'Public');
-    const filePath = path.join(publicDir, req.url === '/' ? 'index.html' : req.url);
+    const publicDir = path.join(baseDir, "Public");
+    const filePath = path.join(
+      publicDir,
+      req.url === "/" ? "index.html" : req.url,
+    );
     const fileExtension = path.extname(filePath);
     const contentTypeValue = contentType(fileExtension);
 
-    const fileContent = await fs.readFile(filePath, 'utf-8');
+    const fileContent = await fs.readFile(filePath, "utf-8");
     res.statusCode = 200;
-    res.setHeader('Content-Type', contentTypeValue);
+    res.setHeader("Content-Type", contentTypeValue);
     res.end(fileContent);
   } catch (error) {
-    console.error('Error serving file:', error.message);
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('404 - File Not Found');
+    if (error.code === "ENOENT") {
+      const errorPath = path.join(baseDir, "Public", "404.html");
+      const errorContent = await fs.readFile(errorPath, "utf-8");
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      res.end(errorContent);
+      return;
+    } else {
+      console.error("Error serving file:", error.message);
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("404 - File Not Found");
+    }
   }
 }
