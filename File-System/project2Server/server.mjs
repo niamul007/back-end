e0f18f7eef0c4fe25e09169c5d9c5ddcf48a3b2c
niamul007 <am3994566@gmail.com>
2026-01-31 19:37:45 +0600

    adding import dat

---
+++ File-System/project2Server/server.mjs
@@ -0,0 +1,33 @@
+import express from "express";
+import path from "node:path";
+import { readPost } from "../Utilities/postHandelers.js";
+import { writePost } from "../Utilities/postHandelers.js";
+import { error } from "node:console";
+
+const app = express();
+const PORT = 4001;
+const __dirname = import.meta.dirname;
+
+app.use(express.static(path.join(__dirname, "..", "project2")));
+app.use(express.json());
+
+app.get("/name", async (req, res) => {
+  const names = await readPost(); // FIX: Get the data first
+  res.json(names); // Then send it
+});
+
+app.post("/add-task", async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ error: "Missing data" });
+    const readName = await readPost();
+    readName.push({ name, id: Date.now() });
+    await writePost(readName);
+    res.status(201).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error: "save failed" });
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Hi alaka bashi this is your url: http://localhost:${PORT}`);
+});
