81b6bb79ef63c2a9b0a96c800fc8c7612d42e69d
niamul007 <am3994566@gmail.com>
2026-02-01 21:00:09 +0600

    adding get and post method

--- File-System/pr3/server.mjs
+++ File-System/pr3/server.mjs
@@ -1,21 +1,39 @@
-import express from "express"
-import path from 'node:path'
+import express from "express";
+import path from "node:path";
 import { readPost } from "../Utilities/postHandelers.js";
 import { writePost } from "../Utilities/postHandelers.js";
-
+import { error } from "node:console";
 
 const app = express();
 const PORT = 7001;
 const __dirname = import.meta.dirname;
 
-app.use(express.static(path.join(__dirname,"..","pr3Html")));
+app.use(express.static(path.join(__dirname, "..", "pr3Html")));
 app.use(express.json());
 
-app.get("/", async (req,res)=>{
-    const read = await readPost()
-    res.json()
-})
+app.get("/expense", async (req, res) => {
+  const read = await readPost();
+  res.json(read);
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server running on: http://localhost:${PORT}`)
-})
+app.post("/add-expense", async (req, res) => {
+  try {
+    const { cause, amount } = req.body;
+    if (!cause || !amount)
+      return res.status(400).json({ error: "Missing data" });
+    const read = await readPost();
+    read.push({
+      cause: cause,
+      amount: amount,
+      id: Date.now(),
+    });
+    await writePost(read);
+    res.status(201).json({ success: true });
+  } catch (err) {
+    res.status(500).json({ error: "Save failed" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on: http://localhost:${PORT}`);
+});
