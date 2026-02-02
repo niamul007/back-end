000453fd519d3385096fffe719bf8382e395ebeb
niamul007 <am3994566@gmail.com>
2026-02-02 19:54:32 +0600

    adding important data

--- File-System/pr3/server.mjs
+++ File-System/pr3/server.mjs
@@ -3,6 +3,7 @@ import path from "node:path";
 import { readPost } from "../Utilities/postHandelers.js";
 import { writePost } from "../Utilities/postHandelers.js";
 import { error } from "node:console";
+import e from "express";
 
 const app = express();
 const PORT = 7001;
@@ -36,14 +37,37 @@ app.post("/add-expense", async (req, res) => {
 
 app.delete("/delete-expense/:id", async (req, res) => {
   try {
-    const {id} = req.params;
-    const idToDel = Number(id)
+    const { id } = req.params;
+    const idToDel = Number(id);
     const read = await readPost();
     const update = read.filter((i) => i.id !== idToDel);
     await writePost(update);
     res.json({ success: true });
   } catch (err) {
     res.status(500).json({ error: "Delete failed" });
+  }
+});
+
+app.put("/update-expense/:id", async (req, res) => {
+  // Added /
+  try {
+    const { id } = req.params;
+    const { cause } = req.body;
+    const data = await readPost();
+
+    const updateEx = data.map((ex) => {
+      if (ex.id == id) {
+        // Used == for type safety
+        return { ...ex, cause: cause };
+      }
+      return ex; // Return the individual item, not the whole array
+    });
+
+    await writePost(updateEx);
+    res.json({ success: true });
+  } catch (err) {
+    console.log(`Problem: ${err}`);
+    res.status(500).send("Error");
   }
 });
 
