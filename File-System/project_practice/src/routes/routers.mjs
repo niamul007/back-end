e3addf616e1b50bc8b9ad7ca8e19804df3490824
niamul007 <am3994566@gmail.com>
2026-02-05 20:54:31 +0600

    adding routers

--- File-System/project_practice/src/routes/routers.mjs
+++ File-System/project_practice/src/routes/routers.mjs
@@ -1,8 +1,9 @@
 import express from "express";
 import { getItem, postItem } from "../controller/itemController.mjs";
+import { validator } from "../middleware/validator.mjs";
 
 const router = express.Router();
 router.get("/items",getItem)
-router.post("/add-items", postItem);
+router.post("/add-items",validator,postItem);
 
 export default router;
