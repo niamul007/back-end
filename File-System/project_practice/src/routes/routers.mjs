8d87f476bd7b13b0874f89bc45d2123a50516493
niamul007 <am3994566@gmail.com>
2026-02-05 21:36:33 +0600

    adding del fn

--- File-System/project_practice/src/routes/routers.mjs
+++ File-System/project_practice/src/routes/routers.mjs
@@ -1,9 +1,9 @@
 import express from "express";
-import { getItem, postItem } from "../controller/itemController.mjs";
+import { getItem, postItem, delItem } from "../controller/itemController.mjs";
 import { validator } from "../middleware/validator.mjs";
 
 const router = express.Router();
-router.get("/items",getItem)
-router.post("/add-items",validator,postItem);
-
+router.get("/items", getItem);
+router.post("/add-items", validator, postItem);
+router.delete("/delete-item/:id", delItem);
 export default router;
