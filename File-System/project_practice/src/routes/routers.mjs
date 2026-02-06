d72c3861043014b17f938898e50e4bcd303a6945
niamul007 <am3994566@gmail.com>
2026-02-06 17:56:23 +0600

    adding put route

--- File-System/project_practice/src/routes/routers.mjs
+++ File-System/project_practice/src/routes/routers.mjs
@@ -1,9 +1,10 @@
 import express from "express";
-import { getItem, postItem, delItem } from "../controller/itemController.mjs";
+import { getItem, postItem, delItem,updateItem } from "../controller/itemController.mjs";
 import { validator } from "../middleware/validator.mjs";
 
 const router = express.Router();
 router.get("/items", getItem);
 router.post("/add-items", validator, postItem);
 router.delete("/delete-item/:id", delItem);
+router.put("/edit/:id", updateItem);
 export default router;
