7cecfc07f9982cf18f8f4adb42d11788dbbd7967
niamul007 <am3994566@gmail.com>
2026-02-06 18:41:53 +0600

    adding routes

--- File-System/project_practice/src/routes/routers.mjs
+++ File-System/project_practice/src/routes/routers.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { getItem, postItem, delItem,updateItem } from "../controller/itemController.mjs";
+import { getItem, postItem, delItem,updateItem ,toggleItem,searchItems} from "../controller/itemController.mjs";
 import { validator } from "../middleware/validator.mjs";
 
 const router = express.Router();
@@ -7,4 +7,6 @@ router.get("/items", getItem);
 router.post("/add-items", validator, postItem);
 router.delete("/delete-item/:id", delItem);
 router.put("/edit/:id", updateItem);
+router.patch("/toggle/:id", toggleItem); // We use PATCH for small updates
+router.get("/search", searchItems);
 export default router;
