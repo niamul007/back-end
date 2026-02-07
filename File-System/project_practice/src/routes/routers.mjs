016632c970c628c3e8f86223329557eb14585301
niamul007 <am3994566@gmail.com>
2026-02-07 20:41:56 +0600

    adding new fn

--- File-System/project_practice/src/routes/routers.mjs
+++ File-System/project_practice/src/routes/routers.mjs
@@ -1,10 +1,11 @@
 import express from "express";
 import { getItem, postItem, delItem,updateItem ,toggleItem,searchItems} from "../controller/itemController.mjs";
-import { validator } from "../middleware/validator.mjs";
+import { validate } from "../middleware/validator.mjs";
+import { grocerySchema } from "../middleware/itemValidator.mjs";
 
 const router = express.Router();
 router.get("/items", getItem);
-router.post("/add-items", validator, postItem);
+router.post("/add-items", validate(grocerySchema), postItem);
 router.delete("/delete-item/:id", delItem);
 router.put("/edit/:id", updateItem);
 router.patch("/toggle/:id", toggleItem); // We use PATCH for small updates
