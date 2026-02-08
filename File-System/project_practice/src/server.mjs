684a9f83b2c11d2637ff6a90513ded6e2f958b29
niamul007 <am3994566@gmail.com>
2026-02-08 19:33:05 +0600

    adding global error

--- File-System/project_practice/src/server.mjs
+++ File-System/project_practice/src/server.mjs
@@ -2,6 +2,8 @@ import express from "express";
 import path from "node:path";
 import { initStorage } from "./model/models.mjs";
 import router from "./routes/routers.mjs";
+import AppError from "./utils/appError.mjs";
+import { globalErrorHandler } from "./middleware/errorMiddleware.mjs";
 
 const __dirname = import.meta.dirname;
 const app = express();
@@ -11,7 +13,14 @@ const filePath = path.join(__dirname, "..", "public");
 app.use(express.json());
 app.use(express.static(filePath));
 app.use("/api", router);
+// ... your routes ...
 
+// 1. Handle "Not Found" routes
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+// 2. The Global Error Handler (Must be at the bottom)
+app.use(globalErrorHandler);
 initStorage()
   .then(() => {
     app.listen(PORT, () => {
