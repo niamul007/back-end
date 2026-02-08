c1f9fe0b2e3ae85b89c2432b6111ff77eede4160
niamul007 <am3994566@gmail.com>
2026-02-08 19:28:43 +0600

    adding validator

--- File-System/project_practice/src/middleware/itemValidator.mjs
+++ File-System/project_practice/src/middleware/itemValidator.mjs
@@ -1,8 +1,9 @@
-import { z } from 'zod';
+
+import {z} from 'zod'
 
 export const grocerySchema = z.object({
   body: z.object({
-    item: z.string().min(2, "Item name too short"),
-    itemQty: z.number().positive("Quantity must be positive"),
+    item: z.string().min(2,"Item is too short").max(100,"it is too big to be an item"),
+    itemQty: z.number().positive("Quantity must be positive")
   })
-});
+})
