1ead3dcbd75741e43e2985d12607f2e1257a3686
niamul007 <am3994566@gmail.com>
2026-02-10 19:56:56 +0600

    adding route for new item

--- File-System/project_practice/src/routes/routers.mjs
+++ File-System/project_practice/src/routes/routers.mjs
@@ -1,7 +1,8 @@
 import express from "express";
-import { getItem, postItem, delItem,updateItem ,toggleItem,searchItems} from "../controller/itemController.mjs";
+import { getItem, postItem, delItem,updateItem ,toggleItem,searchItems , resetItems} from "../controller/itemController.mjs";
 import { validate } from "../middleware/validator.mjs";
 import { grocerySchema } from "../middleware/itemValidator.mjs";
+
 
 const router = express.Router();
 router.get("/items", getItem);
@@ -10,4 +11,5 @@ router.delete("/delete-item/:id", delItem);
 router.put("/edit/:id", updateItem);
 router.patch("/toggle/:id", toggleItem); // We use PATCH for small updates
 router.get("/search", searchItems);
+router.delete("/search/reset",resetItems);
 export default router;
