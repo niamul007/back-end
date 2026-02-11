0c07f69067e682d192dbe10e83741ca7161f47f2
niamul007 <am3994566@gmail.com>
2026-02-11 22:15:28 +0600

    adding chef for the aiter i mean controller

---
+++ File-System/tracker/src/service/habitService.mjs
@@ -0,0 +1,20 @@
+import { readData, writeData } from "../model/habitModel.mjs";
+import crypto from "crypto";
+
+export const showAllHabit = async () => {
+  const data = await readData();
+  return data;
+};
+
+export const addHabit = async (task) => {
+    const data = await readData();
+    const newData = {
+      id: crypto.randomUUID(),
+      task, // shorthand for task: task
+      isDone: false,
+    };
+    
+    data.push(newData); // This modifies the 'data' array in place
+    await writeData(data); // Save the actual array ('data'), not the result of .push()
+    return newData; // Return the new item so the Controller can show it
+};
