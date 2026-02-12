77c3d5b1d2ed9ed5505edecde8b3714818d6c56a
niamul007 <am3994566@gmail.com>
2026-02-12 19:15:26 +0600

    adding toggle routes

--- File-System/tracker/src/routes/routes.mjs
+++ File-System/tracker/src/routes/routes.mjs
@@ -1,4 +1,4 @@
-import { getAllData, postData , removeItem  } from "../controller/habitController.mjs";
+import { getAllData, postData , removeItem ,toggleData  } from "../controller/habitController.mjs";
 import express from 'express';
 import { validate } from "../middleware/validator.mjs";
 import { taskSchema } from "../middleware/habitValidator.mjs";
@@ -8,5 +8,6 @@ const router = express.Router();
 router.get("/get-task", getAllData);
 router.post("/add-task", validate(taskSchema),postData);
 router.delete("/delete-task/:id", removeItem)
+router.patch("/toggle-task/:id", toggleData)
 
 export default router;
