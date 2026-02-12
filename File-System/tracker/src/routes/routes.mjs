9b179edf95b892e0069a0d04b143044377c3dfb1
niamul007 <am3994566@gmail.com>
2026-02-12 18:57:01 +0600

    adding del route

--- File-System/tracker/src/routes/routes.mjs
+++ File-System/tracker/src/routes/routes.mjs
@@ -1,4 +1,4 @@
-import { getAllData, postData  } from "../controller/habitController.mjs";
+import { getAllData, postData , removeItem  } from "../controller/habitController.mjs";
 import express from 'express';
 import { validate } from "../middleware/validator.mjs";
 import { taskSchema } from "../middleware/habitValidator.mjs";
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.get("/get-task", getAllData);
 router.post("/add-task", validate(taskSchema),postData);
+router.delete("/delete-task/:id", removeItem)
 
 export default router;
