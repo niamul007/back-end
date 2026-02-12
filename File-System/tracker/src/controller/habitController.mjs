9329e03d731cd59536eef50c09e0b2f44cd8cf72
niamul007 <am3994566@gmail.com>
2026-02-12 18:56:18 +0600

    adding del btn

--- File-System/tracker/src/controller/habitController.mjs
+++ File-System/tracker/src/controller/habitController.mjs
@@ -1,24 +1,39 @@
+import AppError from "../../../project_practice/src/utils/appError.mjs";
 import * as habitService from "../service/habitService.mjs";
 import catchAsync from "../utility/catchAsync.mjs";
 
 export const getAllData = catchAsync(async (req, res, next) => {
   const data = await habitService.showAllHabit();
-  
+
   // Wrap it so the Frontend knows where to look
   return res.status(200).json({
     status: "success",
-    data: data 
+    data: data,
   });
 });
 
-
 export const postData = catchAsync(async (req, res, next) => {
-    // 1. Get data from service
-    const newItem = await habitService.addHabit(req.body.task);
-    
-    // 2. Send structured response
-    res.status(201).json({
-        status: "success",
-        data: newItem
-    });
+  // 1. Get data from service
+  const newItem = await habitService.addHabit(req.body.task);
+
+  // 2. Send structured response
+  res.status(201).json({
+    status: "success",
+    data: newItem,
+  });
 });
+
+export const removeItem = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const delitem = await habitService.delHabit(id);
+    res.status(200).json({
+      status: "success",
+      data: delitem,
+    });
+  } catch (err) {
+    if (err.message === "NOT_FOUND")
+      return next(new AppError("Not found", 400));
+    throw err;
+  }
+};
