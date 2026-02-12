f7c73a15d6eb958cee74811b15e95181096b7ed8
niamul007 <am3994566@gmail.com>
2026-02-12 19:14:49 +0600

    adding togle for done

--- File-System/tracker/src/service/habitService.mjs
+++ File-System/tracker/src/service/habitService.mjs
@@ -7,26 +7,40 @@ export const showAllHabit = async () => {
 };
 
 export const addHabit = async (task) => {
-    const data = await readData();
-    const newData = {
-      id: crypto.randomUUID(),
-      task, // shorthand for task: task
-      isDone: false,
-    };
-    
-    data.push(newData); // This modifies the 'data' array in place
-    await writeData(data); // Save the actual array ('data'), not the result of .push()
-    return newData; // Return the new item so the Controller can show it
+  const data = await readData();
+  const newData = {
+    id: crypto.randomUUID(),
+    task, // shorthand for task: task
+    isDone: false,
+  };
+
+  data.push(newData); // This modifies the 'data' array in place
+  await writeData(data); // Save the actual array ('data'), not the result of .push()
+  return newData; // Return the new item so the Controller can show it
 };
 
-
-export const delHabit = async (id) =>{
+export const delHabit = async (id) => {
   const data = await readData();
   const update = data.filter((item) => item.id !== id);
 
-  if(update.length === data.length){
-    throw new Error("Not found")
+  if (update.length === data.length) {
+    throw new Error("Not found");
   }
-  await writeData(update)
+  await writeData(update);
   return update;
-}
+};
+
+export const toggleItem = async (id) => {
+  const data = await readData();
+  if (!data.find((i) => i.id === id)) {
+    throw new Error("NOT_FOUND");
+  }
+  const updated = data.map((item) => {
+    if (item.id === id) {
+      return { ...item, isDone: !item.isDone };
+    }
+    return item;
+  });
+  await writeData(updated);
+  return updated;
+};
